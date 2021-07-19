ca08b0f518e6d14f8ba6e4f4e0ce8a73e649ba07
Haim <hgq_2000@hotmail.com>
2021-07-19 17:04:42 -0500

    add products class

---
+++ prueba/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prueba
+{
+    class Product
+    {
+        //private int productoID;
+        //private int ubicacionID = null;
+        //private int unidadID = null;
+
+        //private int cantidad = null;
+        //private string fecha = null;
+        //private string usuario = null;
+
+        public int productoID{
+            get { return productoID; }
+            set { productoID = value; }
+        }
+        public int ubicacionID {
+            get { return ubicacionID; }
+            set { ubicacionID = value; }
+        }
+        public int unidadID {
+            get { return unidadID; }
+            set { unidadID = value; }
+        }
+        public string fecha {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+        public string usuario {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+        public int cantidad {
+            get { return cantidad;}
+            set { cantidad = value; }
+        }
+    }
+}
